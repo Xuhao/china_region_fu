0e448139a912a8b6479198f4f0a7d2b0ea6b72cd
Xuhao <xuhao@rubyfans.com>
2012-06-08 17:42:37 +0800

    Improve Migration file

--- lib/generators/china_region_fu/install/templates/migration.rb
+++ lib/generators/china_region_fu/install/templates/migration.rb
@@ -1,45 +1,48 @@
 class CreateChinaRegionTables < ActiveRecord::Migration
-  def self.up
-    create_table :provinces do |t|
-      t.string :name
-      t.string :pinyin
-      t.string :pinyin_abbr
-      t.timestamps
+  def change
+    unless table_exists? 'provinces'
+      create_table :provinces do |t|
+        t.string :name
+        t.string :pinyin
+        t.string :pinyin_abbr
+        t.timestamps
+      end
+      
+      add_index :provinces, :name
+      add_index :provinces, :pinyin
+      add_index :provinces, :pinyin_abbr
     end
-    create_table :cities do |t|
-      t.string :name
-      t.integer :province_id
-      t.integer :level
-      t.string :zip_code
-      t.string :pinyin
-      t.string :pinyin_abbr
-      t.timestamps
-    end    
-    create_table :districts do |t|
-      t.string :name
-      t.integer :city_id
-      t.string :pinyin
-      t.string :pinyin_abbr
-      t.timestamps
+    
+    unless table_exists? 'cities'
+      create_table :cities do |t|
+        t.string :name
+        t.integer :province_id
+        t.integer :level
+        t.string :zip_code
+        t.string :pinyin
+        t.string :pinyin_abbr
+        t.timestamps
+      end
+      add_index :cities, :name
+      add_index :cities, :province_id
+      add_index :cities, :level
+      add_index :cities, :zip_code
+      add_index :cities, :pinyin
+      add_index :cities, :pinyin_abbr
     end
-    add_index :districts, :name
-    add_index :districts, :city_id
-    add_index :districts, :pinyin
-    add_index :districts, :pinyin_abbr
-    add_index :cities, :name
-    add_index :cities, :province_id
-    add_index :cities, :level
-    add_index :cities, :zip_code
-    add_index :cities, :pinyin
-    add_index :cities, :pinyin_abbr
-    add_index :provinces, :name
-    add_index :provinces, :pinyin
-    add_index :provinces, :pinyin_abbr
+    
+    unless table_exists? 'districts'  
+      create_table :districts do |t|
+        t.string :name
+        t.integer :city_id
+        t.string :pinyin
+        t.string :pinyin_abbr
+        t.timestamps
+      end
+      add_index :districts, :name
+      add_index :districts, :city_id
+      add_index :districts, :pinyin
+      add_index :districts, :pinyin_abbr
+    end
   end
-
-
-  def self.down
-    drop_table :provinces, :cities, :districts
-  end
-
 end
