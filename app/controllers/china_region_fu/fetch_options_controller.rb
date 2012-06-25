aaa7bfd22d1d5f3d750627ca63aca36a120579f3
Xuhao <xuhao@rubyfans.com>
2012-06-25 14:48:34 +0800

    Order the drop down options

--- app/controllers/china_region_fu/fetch_options_controller.rb
+++ app/controllers/china_region_fu/fetch_options_controller.rb
@@ -5,6 +5,11 @@ module ChinaRegionFu
       if params_valid?(params) and parent_klass = params[:parent_klass].classify.safe_constantize.find(params[:parent_id])
         table_name = params[:klass].tableize
         regions = parent_klass.__send__(table_name).select("#{table_name}.id, #{table_name}.name")
+        if has_level_column?(params[:klass])
+          regions = regions.order('level ASC')
+        else
+          regions = regions.order('name ASC')
+        end
         self.response_body = regions.to_json
       else
         self.response_body = [].to_json
@@ -13,6 +18,10 @@ module ChinaRegionFu
     
     
     private
+    
+    def has_level_column?(klass_name)
+      klass_name.classify.safe_constantize.try(:column_names).to_a.include?('level')
+    end
     
     def params_valid?(params)
       params[:klass].present? and params[:parent_klass] =~ /^province|city$/i and params[:parent_id].present?
