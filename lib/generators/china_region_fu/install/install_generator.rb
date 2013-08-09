20faf88508e6b34d938616a4a84b9bed99c2ce79
Xuhao <xuhao@rubyfans.com>
2013-08-09 18:19:28 +0800

    MISC - china_region_fu:install generator

--- lib/generators/china_region_fu/install/install_generator.rb
+++ lib/generators/china_region_fu/install/install_generator.rb
@@ -7,14 +7,24 @@ module ChinaRegionFu
       migration_template "migration.rb", "db/migrate/create_china_region_tables.rb"
     end
 
-    desc "Download https://github.com/Xuhao/china_region_data/raw/master/regions.yml to config/regions.yml."
-    def download_region_config_file
-      get 'https://github.com/Xuhao/china_region_data/raw/master/regions.yml', 'config/regions.yml'
-    end
-
     def execute_migrate
       rake("db:migrate")
     end
+
+    desc "Download https://github.com/Xuhao/china_region_data/raw/master/regions.yml to config/regions.yml."
+    def download_region_config_file
+      get 'https://github.com/Xuhao/china_region_data/raw/master/regions.yml', 'config/regions.yml'
+    rescue
+      puts "\e[31mWarnning!!!\e[0m"
+      puts "Download \e[33mregions.yml\e[0m failed!"
+      puts ''
+      puts 'You need do:'
+      puts "1. download: \e[33mhttps://github.com/Xuhao/china_region_data/raw/master/regions.yml\e[0m"
+      puts '2. put it to config/ direcotry.'
+      puts "3. run: \e[32mrake region:import\e[0m"
+      abort
+    end
+
 
     def import_region_to_data
       rake('region:import')
